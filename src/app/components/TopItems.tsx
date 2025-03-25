ba5f65ca14ef27765bda6ccad4a67084fc99b65f
mgz11 <mgonz112000@gmail.com>
2025-03-25 12:44:31 -0700

    updated styling error with images

--- src/app/components/TopItems.tsx
+++ src/app/components/TopItems.tsx
@@ -47,31 +47,37 @@ export default function TopItems({ items, searchType }: TopItemsProps) {
 				Your Top {searchType.charAt(0).toUpperCase() + searchType.slice(1)}:
 			</h2>
 			<ul className="space-y-4 w-full">
-				<div className="grid grid-cols-3 gap-4 mb-6">
+				<div className="grid grid-cols-3 gap-4 mb-6 items-stretch">
 					{items.slice(0, 3).map((item, index) => {
 						const images = getImages(item);
 						return (
-							<li key={index} className="flex flex-col items-center">
-								<p className="text-lg font-semibold mb-2">{index + 1}</p>
-								{images[1] && (
-									<Image
-										src={images[1].url}
-										width={images[1].width || 100}
-										height={images[1].height || 100}
-										alt="Picture of the album/artist"
-										className="rounded shadow"
-									/>
-								)}
-								<p className="mt-2 text-center font-medium">{item.name}</p>
-								{item.artists && searchType === "tracks" && (
-									<p className="text-sm text-gray-600">
-										{item.artists.map((artist) => artist.name).join(", ")}
-									</p>
-								)}
+							<li
+								key={index}
+								className="flex flex-col justify-between items-center text-center h-full list-none p-4 text-sm md:text-xl"
+							>
+								<div className="flex flex-col items-center">
+									<p className="text-lg font-semibold mb-2">{index + 1}</p>
+									{images[1] && (
+										<Image
+											src={images[1].url}
+											width={images[1].width || 100}
+											height={images[1].height || 100}
+											alt="Picture of the album/artist"
+											className="rounded shadow"
+										/>
+									)}
+									<p className="mt-2 text-center font-medium">{item.name}</p>
+									{item.artists && searchType === "tracks" && (
+										<p className="text-sm text-gray-600 text-center">
+											{item.artists.map((artist) => artist.name).join(", ")}
+										</p>
+									)}
+								</div>
 								<a
 									href={item.external_urls.spotify}
 									target="_blank"
 									rel="noopener noreferrer"
+									className="mt-4"
 								>
 									<Image
 										src="/assets/Spotify_Primary_Logo.png"
